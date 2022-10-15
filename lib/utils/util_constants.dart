bb37de8f3476f7ed27347111293ab3af05dd0b41
bipin5711 <dubeybipin5711@gmail.com>
2022-10-15 12:56:28 +0530

    all constants added

--- lib/utils/util_constants.dart
+++ lib/utils/util_constants.dart
@@ -33,5 +33,130 @@ const String strButcher = 'Butcher';
 const String strAnd = 'and';
 // const String str = '';
 
+// Flow 2
+const String strRiverlandsRanch = 'Riverlands Ranch';
+const String strHouston = 'Houston';
+const String strSeeReviews = 'See Reviews';
+const String strMi = 'mi.';
+const String strViewDirections = 'View Directions';
+const String strPaymentOptions = 'Payment Options';
+const String strCash = 'Cash';
+const String strZelle = 'Zelle';
+const String strCheck = 'Check';
+const String strBankTransfer = 'Bank Transfer';
+const String strCrypto = 'Crypto';
+const String strCreditCard = 'Credit Card';
+const String strDescription = 'Description';
+const String strLorem =
+    'Lorem ipsum dolor sit amet. Et enim neque sit illum similique eum ipsa architecto sed omnis aliquid et distinctio maxime sit voluptates dolorem. Ut dolores ipsum qui distinctio magni et fugiat odit. Et consequatur commodi quo enim voluptates eos odio.';
+const String strLinkWithFarmer = 'Link with a Farmer';
+const String strHi = 'Hi';
+const String strMapView = 'Map View';
+const String strFilter = 'Filter';
+const String strSearchCity = 'Search City';
+const String strViewProfile = 'View Profile';
+const String strViewInventory = 'View Inventory';
+const String strLivestock = 'Livestock';
+const String strClearAll = 'Clear all';
+const String strBeef = 'Beef';
+const String strPork = 'Pork';
+const String strFowl = 'Fowl';
+const String strSheep = 'Sheep';
+const String strGoat = 'Goat';
+const String strPoultry = 'Poultry';
+const String strRabbit = 'Rabbit';
+const String strWildExotic = 'Wild/Exotic';
+const String strTypeMeat = 'Type of Meat';
+const String strHalal = 'Halal';
+const String strKosher = 'Kosher';
+const String strGrassFed = 'Grass Fed';
+const String strGrainFed = 'Grain Fed';
+const String strHormoneFree = 'Hormone Free';
+const String strCustomFeedProgram = 'Custom Feed Program';
+const String strGMOFree = 'GMO Free';
+const String strDairy = 'Dairy';
+const String strCow = 'Cow';
+const String strMilk = 'Milk';
+const String strCheese = 'Cheese';
+const String strProduce = 'Produce';
+const String strBulkVegetables = 'Bulk Vegetables';
+const String strRetail = 'Retail';
+const String strMiscellaneous = 'Miscellaneous';
+const String strHoney = 'Honey';
+const String strSoaps = 'Soaps';
+const String strEggs = 'Eggs';
+const String strApply = 'Apply';
 
+// Flow 3
+const String strLiketoPurchase = 'Hello I would like to purchase 5 goats. Let me know the ';
+const String strYourTotal = 'Hello, your total is \$700';
+const String strSendOffer = 'Send Offer';
+const String strYourBasket = 'Your Basket';
+const String strAddItem = 'Add Item';
+const String strPlus = '+';
+const String strOrderPlaced = 'Order Placed';
+const String strOrderAccepted = 'Your order has been accepted';
+const String strOrder = 'Order';
+const String strConnectwithButcher = 'Connect with Butcher';
+const String strOrderDeclined = 'Your order has been declined';
+const String strFindAnotherFarm = 'Find another Farm';
+const String strJobComplete = 'Job Complete';
 
+// Flow 4
+const String strCustomerReviews = 'Customer Reviews';
+const String strDanyalGabaji = 'Danyal Gabaji';
+const String strWonderfulExperience = 'Had a wonderful experience and would order from them again.';
+const String str27July = '27 July 2022';
+const String strSearchFavorites = 'Search favorites...';
+const String strSearchItems = 'Search for items at Riverlands Ranch...';
+const String strInStock = 'In Stock';
+const String strAvailability = 'Availability';
+
+// Flow 5
+const String str1836Butchers = '1836 Butchers';
+const String strViewButchersProfile = 'View Butcher\'s Profile';
+const String strAnimalsButchered = 'Animals Butchered';
+const String strCertification = 'Certification';
+const String strStateUSDACertified = 'State USDA Certified';
+const String strCustomExempt = 'Custom Exempt';
+const String strFederalUSDACertified = 'Federal USDA Certified';
+const String str7407Spencer = '7407 Spencer Hwy Ste 160 Pasadena, TX 77505';
+const String strWeekdays = 'Weekdays';
+const String strWeekends = 'Weekends';
+const String strLinkwithButcher = 'Link with a Butcher';
+const String strShareNewJob = 'Share new job';
+const String strShareExistingJob = 'Share existing job';
+const String strEnterOrderNumber = 'Enter Order Number';
+
+// Flow 6
+const String strRiverlands1836Butchers  = 'Riverlands Ranch & 1836 Butchers ';
+const String strOrderwillReady  = 'Hello, let me know when my order will be ready ';
+const String str = 'Your order has been accepted';
+const String strFindAnotherButcher = 'Find another Butcher';
+
+//Flow 7
+const String strChatEnded  = 'Your chat with Riverlands Ranch and 1836 Butchers has ended. ';
+const String strHowExperienceWithButchers = 'How was your experience with 1836 Butchers?';
+const String strAdditonalComments = 'Additonal Comments...';
+const String strSubmit = 'Submit';
+
+// Flow 8
+const String strJonatha = 'Jonatha';
+const String strJanathaGmail = 'Janatha@gmail.com';
+const String strEditProfile = 'Edit Profile';
+const String strOrderHistory = 'Order History';
+const String strGeneral = 'General';
+const String strLogout = 'Logout';
+const String strUsername = 'Username';
+const String strSaveChanges = 'Save Changes';
+const String strUpdateProfilePicture= 'Update New Profile Picture';
+const String strRecentPhotostook = 'Here are the recent photos you took, choose one.';
+const String strSetProfilePicture = 'Set as Profile Picture';
+const String strSearchOrder = 'Search Order...';
+const String strQty = 'Qty';
+const String strTotal = 'Total';
+const String strDateSample = '08/07/2022';
+const String strFrequentlyAskedQuestions = 'Frequently Asked Questions';
+const String strPrivacyPolicy = 'Privacy Policy';
+const String strTermsService = 'Terms of Service';
+const String strYourFeedback = 'Your Feedback';
